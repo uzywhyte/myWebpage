6bf4ff9bc26df501b6db633394f7cd421f1cb84a
Uzoma Ezeson <uzywhyte1@gmail.com>
2020-03-27 00:26:44 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+ let name = "Uzoma";
+ var courses = ["Design", "html", "css" "javascript"];
+
+ console.log("My name is: " + name);
+ console.log("My courses are: " + courses);
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
